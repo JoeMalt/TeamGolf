41210d643769e607b056fa56e987c505193664f2
etherandrius <etherandrius@gmail.com>
2018-02-04 13:56:45 +0000

    removed compiler plugin in favor of a simple properties setting.

--- MixedRealityPDF/src/main/java/MixedRealityPDF/PDFPenAndPaper.java
+++ MixedRealityPDF/src/main/java/MixedRealityPDF/PDFPenAndPaper.java
@@ -13,6 +13,7 @@ import java.awt.Image;
 import java.awt.geom.Point2D;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
