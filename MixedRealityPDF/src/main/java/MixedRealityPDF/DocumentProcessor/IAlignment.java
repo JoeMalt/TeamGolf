9bba862a745209486b8e7ecc7d7f81a1ddd49ff6
Gideon Mills <GydeonMylls@users.noreply.github.com>
2018-02-06 11:53:19 +0000

    Restructured package space, added Pipeline class to pass input docs through pipeline page by page

--- MixedRealityPDF/src/main/java/MixedRealityPDF/DocumentProcessor/IAlignment.java
+++ MixedRealityPDF/src/main/java/MixedRealityPDF/DocumentProcessor/IAlignment.java
@@ -1,8 +1,8 @@
-package MixedRealityPDF.Factory;
+package MixedRealityPDF.DocumentProcessor;
 
 import java.awt.*;
 
-public interface Alignment {
+public interface IAlignment {
 
     /**
      * Aligns as image of a page of the scanned document with an image of the same page from the original document
