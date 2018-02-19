961c440fdd2743a160d5da781603c71e91f07e23
Kotzur <kocagnes@gmail.com>
2018-02-19 15:59:26 +0000

    Added testing data, which has successfully passed (very small sample)

--- MixedRealityPDF/src/main/java/MixedRealityPDF/AnnotationProcessor/Identification/decision_tree.py
+++ MixedRealityPDF/src/main/java/MixedRealityPDF/AnnotationProcessor/Identification/decision_tree.py
@@ -6,21 +6,15 @@ import graphviz
 
 
 def train_tree():
-    data_file = load_data()
-    print("Hello")
-    clf = tree.DecisionTreeClassifier(criterion = 'entropy', max_depth = 5)
+    data_file = load_data("C://Users/kocag/cam/Year2/group_project/TeamGolf/MixedRealityPDF/Data/trainingData.csv")
+
+    clf = tree.DecisionTreeClassifier(criterion = 'gini', max_depth = 5)
     trainX = data_file.loc[:, data_file.columns != 'key']
     trainY = data_file.key
-
-    print("* trainX", trainX, sep="\n", end="\n\n")
-    print("* trainY", trainY, sep="\n", end="\n\n")
-
-
     return clf.fit(trainX, trainY)
 
 
-def load_data():
-    file_path = "C://Users/kocag/cam/Year2/group_project/TeamGolf/MixedRealityPDF/Data/trainingData.csv"
+def load_data(file_path):
     data_file = None
     if os.path.exists(file_path):
         data_file = pd.read_csv(file_path)
@@ -37,4 +31,6 @@ def evaluate(yHat, testY):
 
 if __name__ == '__main__':
     clf = train_tree()
-    
+    testX = load_data("C://Users/kocag/cam/Year2/group_project/TeamGolf/MixedRealityPDF/Data/testingData.csv")
+    pred = predict(clf, testX)
+    print(pred)
