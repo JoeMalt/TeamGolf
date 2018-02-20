da67b4be1fd099509d72b92c81463bdb345d265b
etherandrius <etherandrius@gmail.com>
2018-02-20 11:54:52 +0000

    Reduced basinhopping from a 3 variable to a 2 variable function.

--- MixedRealityPDF/src/main/python/waveMatching/waveMatching.py
+++ MixedRealityPDF/src/main/python/waveMatching/waveMatching.py
@@ -16,19 +16,34 @@ def normalize(X, min, range):
         
 
 def remap(vector):
-    Xs, Ys, Xo = vector
+    Xs, Xo = vector
     print("remap - %s" % str(vector))
     Xs = normalize(Xs, 1/3, 3)
-    Ys = normalize(Ys, 1/4, 4)
     Xo = normalize(Xo, -50, 150)
-    return [Xs, Ys, Xo]
+    return [Xs, Xo]
 
-def error(vector, original, modified): # vector = [X_scale, Y_scale, X_offset
-    Xs, Ys, Xo = vector
+def matchAreas(original, modified):
+    Ao = 0
+    Am = 0
+    for i in original:
+        Ao += i
+    for i in modified:
+        Am += i
+
+    scale = Ao/Am
+
+    for i in range(0, len(modified)):
+        modified[i] *= scale
+
+    return scale
+
+def match_wave(vector, original, modified): # vector = [X_scale, Y_scale, X_offset
+    Xs, Xo = vector
     print("error - %s" % str(vector))
     limitM = len(modified)
     limitO = len(original)
 
+    # transformation preserves the area.
     def getTransform(m):
         m = m - Xo
         k = m / Xs
@@ -38,13 +53,7 @@ def error(vector, original, modified): # vector = [X_scale, Y_scale, X_offset
         k = int(k)
         a = modified[k]   if 0 <= k   and k   < limitM else 0
         b = modified[k+1] if 0 <= k+1 and k+1 < limitM else 0
-        return Ys*(a + ((l*(b-a))/Xs))
-        #else: # Xs <= 1:
-            #m = m - Xo
-            #k = m / Xs
-            #k = int(k)
-            #a = modified[k] if 0 <= k and k < limitM else 0
-            #return Ys*a
+        return (a + ((l*(b-a))/Xs)) / Xs # divide by Xs to keep areas the same
 
     errorV = 0
     for x in range(int(min(0, -Xo)), int(max(limitO, limitM*Xs - Xo))):
@@ -60,12 +69,12 @@ modified1 = [0,0,2,2,4,4,2,0,0]
 
 original2 = [0,0,2,4,4,0,0,1,2,3,4,5,6,7,8,9,0]*4
 modified2 = [0,0,2,4,4,0,0,1,2,3,4,5,6,7,8,9,0]*4
-assert(error([1,1,0], original2, modified2) == 0)
+#assert(error([1,1,0], original2, modified2) == 0)
 #modified2 = modified2[1:]
 
 original3 = [0,0,2,4,4,0,0]
 modified3 = [0,0,1,2,2,0,0]
-assert(error([1,2,0], original3, modified3) == 0)
+#assert(error([1,2,0], original3, modified3) == 0)
 
 if(len(argv) != 3):
     print("Usage: ./wavematching input_file1 input_file2")
@@ -84,9 +93,11 @@ data_B = list(map(int, (data_B if data_B[0].isdigit() else data_B[1:-1]).split('
 data_A = original3
 data_B = modified3
 
-opt_error = lambda x : error(remap(x), data_A, data_B)
+Ys = matchAreas(data_A, data_B)
 
-ret = optimize.basinhopping(opt_error, [0,0,0], niter=3000)
+opt_error = lambda x : match_wave(remap(x), data_A, data_B)
+
+ret = optimize.basinhopping(opt_error, [0,0], niter=1000)
 #print(opt_error(vector))
 
 print("I'm here")
@@ -96,8 +107,8 @@ results = remap(ret.x)
 print("\nRESULTS")
 print("global min x = %s, f(x0) = %.4f\n" % (str(results), ret.fun))
 print("Xs - %.4f" %abs(results[0]))
-print("Ys - %.4f" %abs(results[1]))
-print("Xo - %.4f" %abs(results[2]))
+print("Ys - %.4f" %abs(Ys/results[0]))
+print("Xo - %.4f" %abs(results[1]))
 
 print(opt_error(ret.x))
 
